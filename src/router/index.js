944e5e9775b99316bc064be3211052ea37263c3d
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-05-17 21:24:50 +0400

    feat: routing configured

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+import Home from '../pages/home';
+import Login from '../pages/login';
+import Register from '../pages/register';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/register',
+        element: <Register />,
+    },
+]);
